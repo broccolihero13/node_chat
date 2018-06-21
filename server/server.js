2619eccfa263540c24efc0f62a2a96988f64c90b
Brock Halladay <bhalladay@instructure.com>
2018-06-20 18:30:28 -0600

    socket io installed

--- server/server.js
+++ server/server.js
@@ -1,12 +1,26 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
+
+io.on('connection', (socket)=>{
+  console.log('new user connect');
+
+  socket.on('disconnect', (socket)=>{
+    console.log(`user disconnected`);
+  });
+});
+
+
 
 app.use(express.static(publicPath));
 
-app.listen(port, ()=>{
+server.listen(port, ()=>{
   console.log(`Server is listening on ${port}`);
 });
